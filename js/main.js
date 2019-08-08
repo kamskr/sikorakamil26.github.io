263633008846ec44c9e588e2f0bb853b08b0ffb9
kamilsikora <sikorakamil26@gmail.com>
2019-08-08 22:27:55 +0200

    test

--- js/main.js
+++ js/main.js
@@ -58,8 +58,9 @@ function onStartButtonClick() {
     // Convert raw data bytes to hex values just for the sake of showing something.
     // In the "real" world, you'd use data.getUint8, data.getUint16 or even
     // TextDecoder to process raw data bytes.
-    for (let i = 0; i < value.byteLength; i++) {
-      a.push('0x' + ('00' + value.getUint8(i).toString(16)).slice(-2));
-    }
+    // for (let i = 0; i < value.byteLength; i++) {
+    //   a.push('0x' + ('00' + value.getUint8(i).toString(16)).slice(-2));
+    // }
+    a.push(data.getUint8());
     log('> ' + a.join(' '));
   }
