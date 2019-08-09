08965f8b72f5fc3aff7f0a5235a36e315c3c0d14
kamilsikora <sikorakamil26@gmail.com>
2019-08-09 14:24:26 +0200

    test

--- js/main.js
+++ js/main.js
@@ -1,65 +1,80 @@
 var myCharacteristic;
 
-function onStartButtonClick() {
-  let serviceUuid = '0xFFE0';
-  if (serviceUuid.startsWith('0x')) {
-    serviceUuid = parseInt(serviceUuid);
-  }
+  var deviceName;
 
-  let characteristicUuid = '0xFFE1';
-  if (characteristicUuid.startsWith('0x')) {
-    characteristicUuid = parseInt(characteristicUuid);
-  }
+  function connect() {
+    let serviceUuid = "0000ffe0-0000-1000-8000-00805f9b34fb";
 
-  log('Requesting Bluetooth Device...');
-  navigator.bluetooth.requestDevice({filters: [{services: [serviceUuid]}]})
-  .then(device => {
-    log('Connecting to GATT Server...');
-    return device.gatt.connect();
-  })
-  .then(server => {
-    log('Getting Service...');
-    return server.getPrimaryService(serviceUuid);
-  })
-  .then(service => {
-    log('Getting Characteristic...');
-    return service.getCharacteristic(characteristicUuid);
-  })
-  .then(characteristic => {
-    myCharacteristic = characteristic;
-    return myCharacteristic.startNotifications().then(_ => {
-      log('> Notifications started');
-      myCharacteristic.addEventListener('characteristicvaluechanged',
-          handleNotifications);
-    });
-  })
-  .catch(error => {
-    log('Argh! ' + error);
-  });
-}
+    let characteristicUuid = "0000ffe1-0000-1000-8000-00805f9b34fb";
 
-function onStopButtonClick() {
-  if (myCharacteristic) {
-    myCharacteristic.stopNotifications()
-    .then(_ => {
-      log('> Notifications stopped');
-      myCharacteristic.removeEventListener('characteristicvaluechanged',
-          handleNotifications);
+    navigator.bluetooth.requestDevice({filters: [{services: [serviceUuid]}]})
+    .then(device => {
+      log('Connecting...');
+      deviceName = device.name;
+      return device.gatt.connect();
+    })
+    .then(server => {
+      console.log('Getting Service...');
+      return server.getPrimaryService(serviceUuid);
+    })
+    .then(service => {
+      console.log('Getting Characteristic...');
+      return service.getCharacteristic(characteristicUuid);
+    })
+    .then(characteristic => {
+      myCharacteristic = characteristic;
+      return myCharacteristic.startNotifications().then(_ => {
+        console.log('> Notifications started');
+        log("Connected to: " + deviceName);
+        myCharacteristic.addEventListener('characteristicvaluechanged',
+            handleNotifications);
+      });
     })
     .catch(error => {
-      log('Argh! ' + error);
+      console.log('Argh! ' + error);
     });
   }
-}
 
-function handleNotifications(event) {
-  let value = event.target.value;
-  let a = [];
-  // Convert raw data bytes to hex values just for the sake of showing something.
-  // In the "real" world, you'd use data.getUint8, data.getUint16 or even
-  // TextDecoder to process raw data bytes.
-  for (let i = 0; i < value.byteLength; i++) {
-    a.push('0x' + ('00' + value.getUint8(i).toString(16)).slice(-2));
+  function disconnect() {
+    if (myCharacteristic) {
+      myCharacteristic.stopNotifications()
+      .then(_ => {
+        console.log('> Notifications stopped');
+        log("Disconnected")
+        myCharacteristic.removeEventListener('characteristicvaluechanged',
+            handleNotifications);
+      })
+      .catch(error => {
+        console.log('Argh! ' + error);
+      });
+    }
   }
-  log('> ' + a.join(' '));
-}
+
+  function handleNotifications(event) {
+    let value = event.target.value;
+    log(deviceName + "> " + new TextDecoder().decode(value));
+    console.log('> Received: ' + new TextDecoder().decode(value));
+  }
+
+
+  //to do wysylania to na razie nie potrzebne
+  function send() {
+    var data = document.getElementById("input").value;
+    log("You> " + data);
+    myCharacteristic.writeValue(str2ab(data+"\n"))
+    document.getElementById("input").value = "";
+  }
+
+  
+  function str2ab(str) {
+    var buf = new ArrayBuffer(str.length*2); // 2 bytes for each char
+    var bufView = new Uint16Array(buf);
+    for (var i=0, strLen=str.length; i<strLen; i++) {
+      bufView[i] = str.charCodeAt(i);
+    }
+    return buf;
+  }
+
+  function log(str) {
+    document.getElementById("term").value += str+"\n";
+  }
